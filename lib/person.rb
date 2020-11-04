6875e9d6b434d4ee0d6d8cb7e438509c92666f34
Erin Anderson <e.anderson1226@gmail.com>
2020-11-03 21:49:04 -0500

    commit tree 6fa48b2eb0a851b9b50d43b5988a8d4447dfba92

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,28 @@
 # your code goes here
+class Person
+
+    def initialize(name)
+    end
+
+    def clean
+    end
+
+    def happy
+    end
+
+    def get_paid(salary)
+    end
+
+    def take_bath
+    end
+
+    def work_out
+    end
+
+    def call_friend(instance)
+    end
+
+    def start_conversation(person, topic)
+    end
+
+end
